dde2675124049b0d247e92bbf59405a900fbc145
Muzi <muzi.phage@gmail.com>
2024-05-18 14:07:15 +0200

    Publisher added headers for conditional functionRouter routing

--- src/main/java/com/pluralsight/fastpassui/FastpassUiApplication.java
+++ src/main/java/com/pluralsight/fastpassui/FastpassUiApplication.java
@@ -1,10 +1,15 @@
 package com.pluralsight.fastpassui;
 
+import java.util.ArrayList;
 import java.util.function.Supplier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class FastpassUiApplication {
@@ -13,9 +18,35 @@ public class FastpassUiApplication {
 		SpringApplication.run(FastpassUiApplication.class, args);
 	}
 
-	@Bean
-	public Supplier<FastPassToll> generateTollCharge(){
+	// Produce an infite number of message - 1 per socond default
+
+	// @Bean
+	public Supplier<FastPassToll> generateTollCharge() {
 		return () -> new FastPassToll("800", "1001", 1.05f);
+	}
+
+	// Produce a finite number of messages with different headers
+	@Bean
+	public Supplier<Flux<Message<FastPassToll>>> generateThreeCharges() {
+
+		ArrayList<Message<FastPassToll>> tolls = new ArrayList<>();
+
+		tolls.add(MessageBuilder
+				.withPayload(new FastPassToll("800", "1001", 1.05f))
+				.setHeader("speed", "slow")
+				.build());
+
+		tolls.add(MessageBuilder
+				.withPayload(new FastPassToll("801", "1001", 1.05f))
+				.setHeader("speed", "fast")
+				.build());
+
+		tolls.add(MessageBuilder
+				.withPayload(new FastPassToll("802", "1001", 1.05f))
+				.setHeader("speed", "slow")
+				.build());
+
+		return () -> Flux.fromIterable(tolls);
 	}
 
 }
